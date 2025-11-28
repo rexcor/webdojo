a6f3666b22b46e9b1b347fcff0a4bf40e6e628d1
rexcor <gabrielmdoliveira@gmail.com>
2025-11-27 21:40:24 -0300

    add invalid password testcase

---
+++ web/cypress/e2e/login.cy.js
@@ -0,0 +1,29 @@
+describe("Login", () => {
+  it("Logar com sucesso", () => {
+    cy.visit("localhost:3000");
+    cy.submitLogin("papito@webdojo.com", "katana123");
+
+    cy.get('[data-cy="user-name"]')
+      .should("be.visible")
+      .and("have.text", "Fernando Papito");
+
+    cy.get('[data-cy="welcome-message"]')
+      .should("be.visible")
+      .and(
+        "have.text",
+        "Olá QA, esse é o seu Dojo para aprender Automação de Testes.",
+      );
+  });
+
+  it("Não deve logar com senha inválida", () => {
+    cy.visit("localhost:3000");
+    cy.submitLogin("papito@webdojo.com", "123456");
+    cy.contains("Acesso negado! Tente novamente.").should("be.visible");
+  });
+
+  it("Não deve logar com email inválido", () => {
+    cy.visit("localhost:3000");
+    cy.submitLogin("lasagna@webdojo.com", "katana123");
+    cy.contains("Acesso negado! Tente novamente.").should("be.visible");
+  });
+});
